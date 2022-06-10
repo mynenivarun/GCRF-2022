0ade8e8178bad7531cd9d6ece0c8dbddd9b7b98b
VARUN MYNENI <74002498+varun-0007@users.noreply.github.com>
2022-06-11 03:03:32 +0530

    Update GCRF progress checker.py

--- GCRF progress checker.py
+++ GCRF progress checker.py
@@ -10,49 +10,49 @@ f = soup.find_all(class_='ql-subhead-1 l-mts')
 list = []
 cnt = 0
 for line in f:
-    cnt += 1
     txt = line.text.strip('\n')
-    ap = [cnt,txt]
-    list.append(ap)
+    list.append(txt)
 for word in list:
-    print(word)
-print('Progress :',cnt,'/60')
+    cnt += 1
+    print(cnt,word)
+per = (cnt/60)*100
+print(f'\nProgress : {cnt}/60 ==> {per}%')
 if (cnt==15):
-    print("Milestone #1 Completed(10 Quests & 5 Skill Badges")
-    print("Prizes : Shirt, Buttuon Badge, Pen, Stickers")
+    print("\nMilestone #1 Completed(10 Quests & 5 Skill Badges)")
+    print("\nPrizes : Shirt, Buttuon Badge, Pen, Stickers")
 elif (cnt==30):
-    print("Milestone #2 Completed(20 Quests & 10 Skill Badges")
-    print("Prizes : Sipper, Shirt, Buttuon Badge, Pen, Stickers")
+    print("\nMilestone #2 Completed(20 Quests & 10 Skill Badges)")
+    print("\nPrizes : Sipper, Shirt, Buttuon Badge, Pen, Stickers")
 elif (cnt==45):
-    print("Milestone #3 Completed(30 Quests & 15 Skill Badges")
-    print("Prizes : Bag, Sipper, Shirt, Buttuon Badge, Pen, Stickers")
+    print("\nMilestone #3 Completed(30 Quests & 15 Skill Badges)")
+    print("\nPrizes : Bag, Sipper, Shirt, Buttuon Badge, Pen, Stickers")
 elif (cnt==60):
-    print("Milestone #4 Completed(40 Quests & 20 Skill Badges")
-    print("Prizes : Voucher, Sipper, Shirt, Buttuon Badge, Pen, Stickers")
-    print("You are elgible For Voucher ")
+    print("\nMilestone #4 Completed(40 Quests & 20 Skill Badges)")
+    print("\nPrizes : Voucher, Sipper, Shirt, Buttuon Badge, Pen, Stickers")
+    print("\nYou are elgible For Voucher ")
 elif(cnt>=0 and cnt<=15):
     t = 15-cnt
-    print('complete another ',t,'for reaching Milestone #1')
-    print("MileStone #1 Prizes : Shirt, Buttuon Badge, Pen, Stickers")
+    print('\ncomplete another ',t,'for reaching Milestone #1')
+    print("\nMileStone #1 Prizes : Shirt, Buttuon Badge, Pen, Stickers")
 elif(cnt>=15 and cnt<=30):
-    print("Milestone #1 Completed(10 Quests & 5 Skill Badges")
+    print("\nMilestone #1 Completed(10 Quests & 5 Skill Badges")
     t = 30-cnt
-    print('complete another ',t,'for reaching Milestone #2')
-    print("MileStone #2 Prizes : Sipper, Shirt, Buttuon Badge, Pen, Stickers")
+    print('\ncomplete another ',t,'for reaching Milestone #2')
+    print("\nMileStone #2 Prizes : Sipper, Shirt, Buttuon Badge, Pen, Stickers")
 elif(cnt>=30 and cnt<=45):
-    print("Milestone #1 Completed(10 Quests & 5 Skill Badges")
-    print("Milestone #2 Completed(20 Quests & 10 Skill Badges")
+    print("\nMilestone #1 Completed(10 Quests & 5 Skill Badges")
+    print("\nMilestone #2 Completed(20 Quests & 10 Skill Badges")
     t = 45-cnt
-    print('complete another ',t,'for reaching Milestone #3')
-    print("MileStone #3 Prizes : Bag, Sipper, Shirt, Buttuon Badge, Pen, Stickers")
+    print('\ncomplete another ',t,'for reaching Milestone #3')
+    print("\nMileStone #3 Prizes : Bag, Sipper, Shirt, Buttuon Badge, Pen, Stickers")
 elif(cnt>=45 and cnt<=60):
-    print("Milestone #1 Completed(10 Quests & 5 Skill Badges")
-    print("Milestone #2 Completed(20 Quests & 10 Skill Badges")
-    print("Milestone #3 Completed(30 Quests & 15 Skill Badges")
+    print("\nMilestone #1 Completed(10 Quests & 5 Skill Badges")
+    print("\nMilestone #2 Completed(20 Quests & 10 Skill Badges")
+    print("\nMilestone #3 Completed(30 Quests & 15 Skill Badges")
     t = 60-cnt
-    print('complete another ',t,'for reaching Milestone #4')
-    print("MileStone #4 Prizes : Voucher, Sipper, Shirt, Buttuon Badge, Pen, Stickers")
+    print('\ncomplete another ',t,'for reaching Milestone #4')
+    print("\nMileStone #4 Prizes : Voucher, Sipper, Shirt, Buttuon Badge, Pen, Stickers")
 elif(cnt==0):
-    print('there is no progress kindly start doing your labs')
+    print('\nthere is no progress kindly start doing your labs')
 else:
-    print("Error occured while executing program")
+    print("\nError occured while executing program")
